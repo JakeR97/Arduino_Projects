13fc8a37221e48409adc23c371c5d19dbcf6fd19
Jake Reardon <37781640+JakeR97@users.noreply.github.com>
2018-08-23 18:01:52 -0400

    Add files via upload

---
+++ ColorsTest/ColorsTest.ino
@@ -0,0 +1,20 @@
+int RED = 3;
+int GREEN = 5;
+int BLUE = 9;
+
+void setup() {
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+}
+
+void loop() {
+  analogWrite(RED, FlipVal(255));
+  analogWrite(GREEN, FlipVal(255));
+  analogWrite(BLUE, FlipVal(0));
+}
+
+int FlipVal(int num) {
+  return abs(num-255);
+}
+
