7a9f6e83f0ad503fe15b5c67ee1f5fcaaf2491cf
Jake Reardon <37781640+JakeR97@users.noreply.github.com>
2018-08-23 09:26:21 -0400

    Add files via upload

---
+++ IndividualLightFade.ino.ino
@@ -0,0 +1,34 @@
+int light = 0; // store the current light value
+int RED = 3;
+int GREEN = 10;
+int BLUE = 11;
+
+void setup() {
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+}
+
+void loop() {
+    analogWrite(RED, 255);
+    analogWrite(GREEN, 255);
+    analogWrite(BLUE, 255);
+    for (int i = 255; i > 0; i--) {
+      analogWrite(RED, i);
+      delay(20);
+    }
+    analogWrite(RED, 255);
+    for (int i = 255; i > 0; i--) {
+      analogWrite(GREEN, i);
+      delay(20);
+    }
+    analogWrite(GREEN, 255);
+    for (int i = 255; i > 0; i--) {
+      analogWrite(BLUE, i);
+      delay(20);
+    }
+    analogWrite(RED, 255);
+    analogWrite(BLUE, 255);
+    analogWrite(GREEN, 255);
+    delay(1000);
+}
---
+++ LightDetector.ino
@@ -0,0 +1,19 @@
+int light = 0; // store the current light value
+int RED = 3;
+int BLUE = 10;
+int GREEN = 11;
+
+void setup() {
+    pinMode(RED, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(BLUE, OUTPUT);
+    pinMode(A0, INPUT);
+}
+
+void loop() {
+    light = analogRead(A0);
+    light = abs((light / 4) - 255);
+    analogWrite(RED, light);
+    analogWrite(GREEN, light);
+    analogWrite(BLUE, light);
+}
